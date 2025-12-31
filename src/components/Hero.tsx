0250dc394df59fa6d509a894dca0fa374bfcadb1
Ezio Pappalardo <eziopappalardo98@gmail.com>
2025-12-31 11:52:20 +0100

    fix: rimuovi icone rotte

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -18,19 +18,7 @@ const Hero: React.FC = () => {
         </div>
         <button className="luma-button" onClick={copyEmail}>
           <div className="luma-wrapper">
-            <svg className="luma-icon luma-icon-left" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-              <path d="M12 2L15 8H9L12 2Z" />
-              <path d="M9 8H15V10C15 13 12 16 12 16C12 16 9 13 9 10V8Z" />
-              <path d="M12 16V22" />
-              <path d="M8 22H16" />
-            </svg>
             <span className="luma-text">{t('getInTouch')}</span>
-            <svg className="luma-icon luma-icon-right" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
-              <path d="M12 2L15 8H9L12 2Z" />
-              <path d="M9 8H15V10C15 13 12 16 12 16C12 16 9 13 9 10V8Z" />
-              <path d="M12 16V22" />
-              <path d="M8 22H16" />
-            </svg>
             <div className="luma-stars">
               <div style={{ position: 'absolute', left: '80%', top: '68%', width: '1px', height: '1px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.5 }} />
               <div style={{ position: 'absolute', left: '57%', top: '30%', width: '1px', height: '1px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.6 }} />
