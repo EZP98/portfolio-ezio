c232ac8ad14914132b7e7e97a9dcce21825a1d87
Ezio Pappalardo <eziopappalardo98@gmail.com>
2025-12-31 11:56:01 +0100

    feat: LumaButton con luce rotante corretta

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,14 +20,17 @@ const Hero: React.FC = () => {
           <div className="luma-wrapper">
             <span className="luma-text">{t('getInTouch')}</span>
             <div className="luma-stars">
-              <div style={{ position: 'absolute', left: '80%', top: '68%', width: '1px', height: '1px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.5 }} />
-              <div style={{ position: 'absolute', left: '57%', top: '30%', width: '1px', height: '1px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.6 }} />
-              <div style={{ position: 'absolute', left: '25%', top: '20%', width: '1px', height: '1px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.5 }} />
-              <div style={{ position: 'absolute', left: '22%', top: '60%', width: '1px', height: '1px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.4 }} />
-              <div style={{ position: 'absolute', left: '7%', top: '44%', width: '1.5px', height: '1.5px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.6 }} />
-              <div style={{ position: 'absolute', left: '13%', top: '75%', width: '1px', height: '1px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.5 }} />
+              <div style={{ position: 'absolute', left: '74.5%', top: '16.5%', width: '0.6px', height: '0.6px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.6 }} />
+              <div style={{ position: 'absolute', left: '3.4%', top: '92%', width: '0.5px', height: '0.5px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.6 }} />
+              <div style={{ position: 'absolute', left: '90.1%', top: '11%', width: '0.57px', height: '0.57px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.6 }} />
+              <div style={{ position: 'absolute', left: '64.6%', top: '81.4%', width: '0.56px', height: '0.56px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.6 }} />
+              <div style={{ position: 'absolute', left: '83.2%', top: '48%', width: '0.88px', height: '0.88px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.6 }} />
+              <div style={{ position: 'absolute', left: '47.7%', top: '35.6%', width: '0.86px', height: '0.86px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.6 }} />
+              <div style={{ position: 'absolute', left: '77.4%', top: '25.9%', width: '0.6px', height: '0.6px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.6 }} />
+              <div style={{ position: 'absolute', left: '49.3%', top: '56.7%', width: '0.89px', height: '0.89px', backgroundColor: '#fff', borderRadius: '50%', opacity: 0.6 }} />
             </div>
           </div>
+          <div className="luma-light" />
         </button>
       </div>
     </section>
