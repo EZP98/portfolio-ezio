9adf87276acd2a0a27ae65940221cdeae805e093
Ezio Pappalardo <eziopappalardo98@gmail.com>
2026-01-01 10:22:38 +0100

    feat: remove 3D gallery from CTA section

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -88,57 +88,6 @@ const CTA: React.FC = () => {
             </button>
           </div>
         </ScrollReveal>
-
-        {/* 3D Cards Gallery */}
-        <div className="cta-gallery">
-          <div className="cta-gallery-perspective">
-            {/* Left Card */}
-            <div className="cta-card cta-card-left">
-              <div className="cta-card-brackets">
-                <span className="bracket bracket-tl"></span>
-                <span className="bracket bracket-tr"></span>
-                <span className="bracket bracket-bl"></span>
-                <span className="bracket bracket-br"></span>
-              </div>
-              <img
-                src="/cta-1.webp"
-                alt="Work sample 1"
-                className="cta-card-image"
-              />
-            </div>
-
-            {/* Center Card */}
-            <div className="cta-card cta-card-center">
-              <div className="cta-card-brackets">
-                <span className="bracket bracket-tl"></span>
-                <span className="bracket bracket-tr"></span>
-                <span className="bracket bracket-bl"></span>
-                <span className="bracket bracket-br"></span>
-              </div>
-              <img
-                src="/cta-2.webp"
-                alt="Work sample 2"
-                className="cta-card-image"
-              />
-            </div>
-
-            {/* Right Card */}
-            <div className="cta-card cta-card-right">
-              <div className="cta-card-brackets">
-                <span className="bracket bracket-tl"></span>
-                <span className="bracket bracket-tr"></span>
-                <span className="bracket bracket-bl"></span>
-                <span className="bracket bracket-br"></span>
-              </div>
-              <img
-                src="/cta-3.webp"
-                alt="Work sample 3"
-                className="cta-card-image"
-              />
-            </div>
-          </div>
-        </div>
-
       </div>
     </section>
   );
